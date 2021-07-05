6ae1d8d6b09c0c48b470861d38cf3456d34cf6e0
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-07-06 03:26:15 +0700

    fix: make models in right state

--- src/main/java/ru/engineeroid/model/Reservation.java
+++ src/main/java/ru/engineeroid/model/Reservation.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "reservation")
@@ -13,12 +14,14 @@ public class Reservation implements IdOwner {
     @GeneratedValue()
     private int id;
 
+    @Column(name = "name")
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "creator_id")
     private User creator;
 
+    @Column(name = "creation_date")
     @CreationTimestamp
     private Timestamp creationDate;
 
@@ -29,8 +32,10 @@ public class Reservation implements IdOwner {
     @JoinColumn(name = "room_id")
     private Room room;
 
-    private Timestamp begin;
-    private Timestamp end;
+    @Column(name = "begin_date")
+    private Timestamp beginDate;
+    @Column(name = "end_date")
+    private Timestamp endDate;
 
     @Override
     public int getId() {
@@ -43,5 +48,83 @@ public class Reservation implements IdOwner {
 
     public void addParticipant(User u) {
         this.participants.add(u);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Timestamp getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Timestamp begin) {
+        this.beginDate = begin;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp end) {
+        this.endDate = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Reservation that = (Reservation) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", creator=" + creator
+                + ", creationDate=" + creationDate
+                + ", room=" + room
+                + ", begin=" + beginDate
+                + ", end=" + endDate
+                + '}';
     }
 }
--- src/main/java/ru/engineeroid/model/Room.java
+++ src/main/java/ru/engineeroid/model/Room.java
@@ -2,6 +2,7 @@ package ru.engineeroid.model;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "room")
@@ -9,6 +10,8 @@ public class Room implements IdOwner {
     @Id
     @GeneratedValue()
     private int id;
+
+    private String name;
 
     @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
     private List<Reservation> reservations;
@@ -20,5 +23,38 @@ public class Room implements IdOwner {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Room room = (Room) o;
+        return id == room.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
     }
 }
